a2c01e81e2fc0b461823c92d143ab40c68e09934
Edward Kang <eyk23@cornell.edu>
2014-01-10 23:56:18 -0500

    initial commit on tree node exercises

---
+++ tree_node.rb
@@ -0,0 +1,61 @@
+class PolyTreeNode
+  include Searchable
+
+  attr_accessor :value, :parent
+
+  def initialize(value = nil)
+    @value, @parent, @children = value, nil, []
+  end
+
+  def children
+    @children.dup
+  end
+
+  def add_child(new_child)
+    @children << new_child
+    new_child.parent = self
+  end
+
+  def remove_child(child)
+    @children.delete(child)
+    child.parent = nil
+  end
+end
+
+module Searchable
+  def dfs(target = nil, &prc)
+    if prc.nil?
+      return self if value == target
+    else
+      return self if prc.call(self)
+    end
+
+    children.each do |child|
+      next if child.nil?
+
+      result = child.dfs(target, &prc)
+      return result unless result.nil?
+    end
+
+    nil
+  end
+
+  def bfs(target = nil, &prc)
+    nodes = [self]
+    until nodes.empty?
+      node = nodes.shift
+
+      if prc.nil?
+        return node if node.value == target
+      else
+        return node if prc.call(node)
+      end
+
+      nodes.concat(node.children)
+    end
+
+    nil
+  end
+end
+
+
